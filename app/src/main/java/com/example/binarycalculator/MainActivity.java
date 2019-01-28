f59bcbd25486c167876ac93d15e74c628f638e0f
Cattusorb <magicmachineplaymc@gmail.com>
2019-01-28 16:48:48 -0500

    Added buttons for o, 1 and clear. Added an input and output text box and created functions for all of them.

--- app/src/main/java/com/example/binarycalculator/MainActivity.java
+++ app/src/main/java/com/example/binarycalculator/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.binarycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s = "";
+    }
+    public void buttonClick(View view) {
+        if(view.getId() == R.id.zero) {
+            s = s + "0";
+        } else if (view.getId() == R.id.one) {
+            s = s + "1";
+        } else if(view.getId() == R.id.clear) {
+            s = "";
+        } else {
+            throw new RuntimeException("Error in buttonClick;; view unknown");
+        }
+        updateView();
+        updateOutputView();
+
+    }
+
+    /**
+     * Updates display to show current count value
+     */
+    private void updateView() {
+        TextView display = findViewById(R.id.input);
+        display.setText(String.format("%s", s));
+    }
+    private void updateOutputView() {
+        if (s.equals("")) {
+            TextView display = findViewById(R.id.output);
+            display.setText(String.format("%s", s));
+
+        } else {
+            Integer i = Integer.parseInt(s, 2);
+            TextView display = findViewById(R.id.output);
+            display.setText(String.format("%d", i));
+        }
     }
 }
